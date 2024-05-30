015c4996053894de9b0a3be57cb7dbc671169303
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-05-30 18:41:21 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,224 @@
+#define NOMINMAX
+#include <Novice.h>
+#include "Vector3.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <assert.h>
+#include <imgui.h>
+#include "Matrix4x4.h"
+#include "Vector3.h"
+#include "VectorMatrix.h"
+
+static const int kWindowWidth = 1280;
+static const int kWindowHeight = 720;
+
+//線分
+struct Segment
+{
+	Vector3 origin;        //始点
+	Vector3 diff;        //終点からの差分
+};
+
+//AABB
+struct AABB
+{
+	Vector3 min; //最小値
+	Vector3 max; //最大値
+};
+
+//Gridを表示する疑似コード
+static void DrawGrid(const Matrix4x4& ViewProjectionMatrix, const Matrix4x4& ViewportMatrix)
+{
+	const float    kGridHalfWidth = 2.0f;                                        //Gridの半分の幅
+	const uint32_t kSubdivision = 10;                                        //分割数
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);    //1つ分の長さ
+
+	//水平方向の線を描画
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; xIndex++)
+	{
+		//X軸上の座標
+		float posX = -kGridHalfWidth + kGridEvery * xIndex;
+
+		//始点と終点
+		Vector3 start = { posX, 0.0f, -kGridHalfWidth };
+		Vector3 end = { posX, 0.0f, kGridHalfWidth };
+		// ワールド座標系 -> スクリーン座標系まで変換をかける
+		start = Transform(start, Multiply(ViewProjectionMatrix, ViewportMatrix));
+		end = Transform(end, Multiply(ViewProjectionMatrix, ViewportMatrix));
+
+		Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, 0x6F6F6FFF);
+	}
+
+	//垂直方向の線を描画
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; zIndex++)
+	{
+		//Z軸上の座標
+		float posZ = -kGridHalfWidth + kGridEvery * zIndex;
+
+		//始点と終点
+		Vector3 startZ = { -kGridHalfWidth, 0.0f, posZ };
+		Vector3 endZ = { kGridHalfWidth, 0.0f, posZ };
+		// ワールド座標系 -> スクリーン座標系まで変換をかける
+		startZ = Transform(startZ, Multiply(ViewProjectionMatrix, ViewportMatrix));
+		endZ = Transform(endZ, Multiply(ViewProjectionMatrix, ViewportMatrix));
+
+		Novice::DrawLine((int)startZ.x, (int)startZ.y, (int)endZ.x, (int)endZ.y, 0x6F6F6FFF);
+	}
+}
+
+//AABBの描画
+void DrawAABB(const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+	Vector3 vertices[8];
+	vertices[0] = { aabb.min.x, aabb.min.y, aabb.min.z };
+	vertices[1] = { aabb.max.x, aabb.min.y, aabb.min.z };
+	vertices[2] = { aabb.min.x, aabb.max.y, aabb.min.z };
+	vertices[3] = { aabb.max.x, aabb.max.y, aabb.min.z };
+	vertices[4] = { aabb.min.x, aabb.min.y, aabb.max.z };
+	vertices[5] = { aabb.max.x, aabb.min.y, aabb.max.z };
+	vertices[6] = { aabb.min.x, aabb.max.y, aabb.max.z };
+	vertices[7] = { aabb.max.x, aabb.max.y, aabb.max.z };
+
+	for (int i = 0; i < 8; ++i)
+	{
+		vertices[i] = Transform(vertices[i], Multiply(viewProjectionMatrix, viewportMatrix));
+	}
+
+	Novice::DrawLine((int)vertices[0].x, (int)vertices[0].y, (int)vertices[1].x, (int)vertices[1].y, color);
+	Novice::DrawLine((int)vertices[0].x, (int)vertices[0].y, (int)vertices[2].x, (int)vertices[2].y, color);
+	Novice::DrawLine((int)vertices[0].x, (int)vertices[0].y, (int)vertices[4].x, (int)vertices[4].y, color);
+	Novice::DrawLine((int)vertices[1].x, (int)vertices[1].y, (int)vertices[3].x, (int)vertices[3].y, color);
+	Novice::DrawLine((int)vertices[1].x, (int)vertices[1].y, (int)vertices[5].x, (int)vertices[5].y, color);
+	Novice::DrawLine((int)vertices[2].x, (int)vertices[2].y, (int)vertices[3].x, (int)vertices[3].y, color);
+	Novice::DrawLine((int)vertices[2].x, (int)vertices[2].y, (int)vertices[6].x, (int)vertices[6].y, color);
+	Novice::DrawLine((int)vertices[3].x, (int)vertices[3].y, (int)vertices[7].x, (int)vertices[7].y, color);
+	Novice::DrawLine((int)vertices[4].x, (int)vertices[4].y, (int)vertices[5].x, (int)vertices[5].y, color);
+	Novice::DrawLine((int)vertices[4].x, (int)vertices[4].y, (int)vertices[6].x, (int)vertices[6].y, color);
+	Novice::DrawLine((int)vertices[5].x, (int)vertices[5].y, (int)vertices[7].x, (int)vertices[7].y, color);
+	Novice::DrawLine((int)vertices[6].x, (int)vertices[6].y, (int)vertices[7].x, (int)vertices[7].y, color);
+}
+
+//AABB衝突判定
+bool IsCollision(const AABB& aabb1, const AABB& aabb2)
+{
+	return (aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) && //x軸
+		(aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) &&
+		(aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z);
+}
+
+const char kWindowTitle[] = "提出用課題";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	AABB aabb1{ .min{-0.5f, -0.5f, -0.5f}, .max{0.0f, 0.0f, 0.0f} };
+	AABB aabb2{ .min{0.2f, 0.2f, 0.2f}, .max{1.0f, 1.0f, 1.0f} };
+
+	Vector3 rotate = {};
+	Vector3 translate = {};
+	Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0)
+	{
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		ImGui::Begin("Settings");
+		ImGui::DragFloat3("AABB1 Min", &aabb1.min.x, 0.01f);
+		ImGui::DragFloat3("AABB1 Max", &aabb1.max.x, 0.01f);
+		ImGui::DragFloat3("AABB2 Min", &aabb2.min.x, 0.01f);
+		ImGui::DragFloat3("AABB2 Max", &aabb2.max.x, 0.01f);
+		ImGui::DragFloat3("rotate", &rotate.x, 0.01f);
+		ImGui::DragFloat3("Camera Translate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("Camera Rotate", &cameraRotate.x, 0.01f);
+		ImGui::End();
+
+		// x
+		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
+		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
+		// y
+		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
+		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
+		// z
+		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.max.z);
+		aabb1.max.z = (std::max)(aabb1.min.z, aabb1.max.z);
+
+		// x
+		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.max.x);
+		aabb2.max.x = (std::max)(aabb2.min.x, aabb2.max.x);
+		// y
+		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.max.y);
+		aabb2.max.y = (std::max)(aabb2.min.y, aabb2.max.y);
+		// z
+		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.max.z);
+		aabb2.max.z = (std::max)(aabb2.min.z, aabb2.max.z);
+
+		//各種行列の計算
+		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewWorldMatrix = Inverse(worldMatrix);
+		Matrix4x4 viewCameraMatrix = Inverse(cameraMatrix);
+
+		// 透視投影行列を作成
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+
+		//ビュー座標変換行列を作成
+		Matrix4x4 viewProjectionMatrix = Multiply(viewWorldMatrix, Multiply(viewCameraMatrix, projectionMatrix));
+
+		//ViewportMatrixビューポート変換行列を作成
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		// Gridを描画
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		// 衝突判定
+		bool collision = IsCollision(aabb1, aabb2);
+
+		// AABBを描画
+		DrawAABB(aabb1, viewProjectionMatrix, viewportMatrix, collision ? 0xFF0000FF : 0xFFFFFFFF);
+		DrawAABB(aabb2, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0)
+		{
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
+
